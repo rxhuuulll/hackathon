7ce7b5bd2fdb58aed19e1522fd1c04c93818b674
RAHUL MK <97874395+rxhuuulll@users.noreply.github.com>
2023-09-12 11:31:24 +0530

    Add files via upload
    first commit

---
+++ ZZ/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var searchButton = document.querySelector("button");
+
+    searchButton.addEventListener("click", function() {
+        window.location.href = "simulation.html";
+    });
+});
+
